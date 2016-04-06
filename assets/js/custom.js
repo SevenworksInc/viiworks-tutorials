7b967104f4457e980bbb00cb3dc8be51df9bedb5
Jesther Bas <dev.jestherbas@hotmail.com>
2016-04-06 12:17:13 +0800

    Initial Files for Development

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	// $('#sidebar_affix').affix({
+		// offset: {
+			// top: 350,
+			// bottom: function () {
+				// return (this.bottom = $('.footer').outerHeight(true))
+			// }
+		// }
+	// })
+});
