f17646a52b48d5cb198729443d46139e08d8d1ad
Jesther Bas <dev.jestherbas@hotmail.com>
2016-04-12 18:45:11 +0800

    More content for instruction

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-	// $('#sidebar_affix').affix({
-		// offset: {
-			// top: 350,
-			// bottom: function () {
-				// return (this.bottom = $('.footer').outerHeight(true))
-			// }
-		// }
-	// })
+	$('#sidebar_affix').affix({
+		offset: {
+			top: 350,
+			// top: $('.nav.nav-pills').offset().top,
+			bottom: function () {
+				return (this.bottom = $('.footer').outerHeight(true))
+			}
+		}
+	})
 });
